89b6469db19725ef3e0ee57d700517e9e8cc7875
Bryon Smith <webmastersmith37@gmail.com>
2022-07-10 14:40:58 -0500

    'minor change'

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,20 +1,40 @@
 pipeline {
-    agent any
+    agent none
     environment {
         CREDS = credentials('bryon_user_password')
     }
     stages {
-        stage('Build') {
+        stage('Get Git') {
+            agent {
+                label 'agent2_node'
+            }
             steps {
-                // Get some code from a GitHub repository
-                checkout([$class: 'GitSCM', branches: [[name: '*/bryon']],
-    userRemoteConfigs: [[url: 'https://github.com/2206-devops-batch/BCEM-Project2.git']]])
+    //             // Get some code from a GitHub repository
+    //             checkout([$class: 'GitSCM', branches: [[name: '*/bryon']],
+    // userRemoteConfigs: [[url: 'https://github.com/2206-devops-batch/BCEM-Project2.git']]])
 
                 // Run Maven on a Unix agent.
                 // sh "mvn -Dmaven.test.failure.ignore=true clean package"
+                sh '. whoami'
                 sh '. $WORKSPACE/temp.sh'
-                sh 'curl -u CREDS https://example.com'
+                // To run Maven on a Windows agent, use
+                // bat "mvn -Dmaven.test.failure.ignore=true clean package"
+            }
 
+            
+        }
+        stage('run shell') {
+            agent {
+                label 'agent2_node'
+            }
+            steps {
+    //             // Get some code from a GitHub repository
+    //             checkout([$class: 'GitSCM', branches: [[name: '*/bryon']],
+    // userRemoteConfigs: [[url: 'https://github.com/2206-devops-batch/BCEM-Project2.git']]])
+
+                // Run Maven on a Unix agent.
+                // sh "mvn -Dmaven.test.failure.ignore=true clean package"
+                sh 'whoami'
                 // To run Maven on a Windows agent, use
                 // bat "mvn -Dmaven.test.failure.ignore=true clean package"
             }
