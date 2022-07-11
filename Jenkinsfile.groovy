af4a5b8e777d91b0d64d26349fe21d3af2edca2d
Bryon Smith <webmastersmith37@gmail.com>
2022-07-10 21:44:21 -0500

    'minor change'

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -11,16 +11,10 @@ pipeline {
             steps {
                 // Get some code from a GitHub repository
                 sh 'whoami'
-                sh 'apt update && apt install git'
                 sh 'git version'
     //             checkout([$class: 'GitSCM', branches: [[name: '*/bryon']],
     // userRemoteConfigs: [[url: 'https://github.com/2206-devops-batch/BCEM-Project2.git']]])
 
-                // Run Maven on a Unix agent.
-                // sh "mvn -Dmaven.test.failure.ignore=true clean package"
-                // sh '. $WORKSPACE/temp.sh'
-                // To run Maven on a Windows agent, use
-                // bat "mvn -Dmaven.test.failure.ignore=true clean package"
             }
 
             
@@ -34,14 +28,8 @@ pipeline {
     //             checkout([$class: 'GitSCM', branches: [[name: '*/bryon']],
     // userRemoteConfigs: [[url: 'https://github.com/2206-devops-batch/BCEM-Project2.git']]])
 
-                // Run Maven on a Unix agent.
-                // sh "mvn -Dmaven.test.failure.ignore=true clean package"
                 sh 'whoami'
-                // To run Maven on a Windows agent, use
-                // bat "mvn -Dmaven.test.failure.ignore=true clean package"
             }
-
-            
         }
     }
 }
