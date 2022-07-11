a853d492805ecb55636d8beb2200dbad368bf43d
Bryon Smith <webmastersmith37@gmail.com>
2022-07-11 09:15:45 -0500

    'minor change'

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -15,6 +15,8 @@ pipeline {
     userRemoteConfigs: [[url: 'https://github.com/2206-devops-batch/BCEM-Project2.git']]])
                 sh '. ./temp.sh'
                 sh "pwd"
+                sh 'python3 -m venv .venv'
+                sh '. .venv/bin/activate'
                 sh 'pip3 install -r ./requirements.txt'
                 sh 'pytest ./test_pytest.py'
 
