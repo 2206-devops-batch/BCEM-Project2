dd318356bcb8af8d9f12b3e87617f2be20930cb7
Bryon Smith <webmastersmith37@gmail.com>
2022-07-11 09:36:34 -0500

    'minor change'

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -20,8 +20,10 @@ pipeline {
                 sh 'python3 -m venv .venv'
                 sh 'ls -al .venv/bin'
                 sh '. .venv/bin/activate'
+                sh 'pip3 list'
                 sh "$path && echo 'hello world!'"
                 sh 'pip3 install -r ./requirements.txt'
+
                 sh 'pytest'
 
             }
